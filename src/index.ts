d4dc22499757aa1d9092b0552aac3e038e266125
marek.bukovsky <marek.bukovsky@asseco-ce.com>
2017-09-04 16:02:29 +0200

    INT: added basic files for illustration

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import zip = require("sample-module");
+import zip3 = require("../types/sample-module2");
+import zip2 = require("sample-module2");
+
+
+var x = new zip();
+var z = new zip2();
+var q = new zip3();
